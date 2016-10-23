35d9c86ce8dda9eb4493e83f5f59ef5dacba2003
azwireth <arora.ujjwal@gmail.com>
2016-10-24 03:19:13 +0530

    Fix some of Vinayak's stupidness

--- app/js/settings.js
+++ app/js/settings.js
@@ -1,6 +1,6 @@
 var previousSelection = "setting";
 
-function setDefaults(){
+function setDefaults() {
 	detectTheme();
 	detectAdvert();
 	detectSearch();
@@ -8,21 +8,20 @@ function setDefaults(){
 	document.getElementById(previousSelection + "Tab").style.color = "#333";
 }
 
-function detectTheme(){
+function detectTheme() {
 	var currTheme = "light"; //TODO: Fixes & Default Values
 	document.getElementById('theme').value = currTheme;
 }
 
-function detectAdvert(){
+function detectAdvert() {
 	var e = document.getElementById("ad");
 	var isTurnedOn = true;
-	if(isTurnedOn){
+	if (isTurnedOn) {
 		e.innerHTML = "On";
 		e.style.background = "#6F6FFF";
 		e.style.color = "#F4F4F4";
 		e.style.border = "none";
-	}
-	else{
+	} else {
 		e.innerHTML = "Off";
 		e.style.background = "#FFF";
 		e.style.color = "#333";
@@ -30,12 +29,12 @@ function detectAdvert(){
 	}
 }
 
-function detectSearch(){
+function detectSearch() {
 	var search = "duckduckgo";
 	document.getElementById("search").value = search;
 }
 
-function setVisible(e){
+function setVisible(e) {
 	var selected = e.id.replace("Tab", "");
 	document.getElementById(previousSelection).style.display = "none";
 	document.getElementById(previousSelection + "Tab").style.color = "#999";
@@ -44,15 +43,14 @@ function setVisible(e){
 	previousSelection = selected;
 }
 
-function toggleAdvert(e){
+function toggleAdvert(e) {
 	console.log(e.innerHTML);
-	if(e.innerHTML == "On"){
+	if (e.innerHTML == "On") {
 		e.innerHTML = "Off";
 		e.style.background = "#FFF";
 		e.style.color = "#333";
 		e.style.border = "1px solid #999";
-	}
-	else{
+	} else {
 		e.innerHTML = "On";
 		e.style.background = "#6F6FFF";
 		e.style.color = "#F4F4F4";
