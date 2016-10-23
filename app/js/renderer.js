0f19dbb49b7ec2c416cdb8c7acd177a6e73a40f0
azwireth <arora.ujjwal@gmail.com>
2016-10-24 03:54:47 +0530

    Add settings and beautify UI

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -22,7 +22,7 @@ $('#search-input').keydown((e) => {
 		if (urlTest.test(url)) {
 			//Add http:// prefix if no prefix is there
 			if (!urlPrefixTest.test(url)) {
-				url = "http://" + url
+				url = 'http://' + url
 			}
 			activeView[0].loadURL(url)
 		}
@@ -59,6 +59,42 @@ $('#add-tab-button').click(() => {
 })
 
 // ===========================
+// Settings Button Function
+// ===========================
+$('#settings-button').click(() => {
+	// Make unactive the currently active tab and view
+	activeView.removeClass('active')
+	activeTab.removeClass('active')
+
+	// Create a tab
+	$('<li class="tab"> \
+		<img class="tab-icon" src="./images/default-favicon.png"/> \
+		<div class="tab-title">Settings</div> \
+		<i class="fa fa-times tab-close-button" title="Close Tab"></i> \
+	</li>').appendTo($('#tabs'))
+	$('.tab:last').addClass('active')
+
+	// Show New Tab page
+	$('<webview class="view" src="./settings.html"></webview>').appendTo($('#views'))
+	$('.view:last').addClass('active')
+
+	// Set actives and add listner events
+	activeView = $('.view.active')
+	activeTab = $('.tab.active')
+	activeTabTitle = $('.tab.active > .tab-title')
+	activeTabIcon = $('.tab.active > .tab-icon')
+	addWebViewListner()
+
+})
+
+// ===========================
+// Home Button Function
+// ===========================
+$('#home-button').click(() => {
+	activeView[0].loadURL(`file://${__dirname}/new-tab.html`)
+})
+
+// ===========================
 // Utility Functions
 // ===========================
 
@@ -66,31 +102,42 @@ $('#add-tab-button').click(() => {
 function addWebViewListner() {
 
 	// Set intermediate loading title, favicon & url
-	activeView[0].addEventListener("load-commit", () => {
-		activeTabTitle.text(activeView[0].getTitle())
-		activeTabIcon.attr('src', "./images/spinner.gif")
-		searchBar.val(activeView[0].getURL())
+	activeView[0].addEventListener('load-commit', () => {
+		// If not local url
+		if(!/^file:\/\//.test(activeView[0].getURL())) {
+			activeTabTitle.text(activeView[0].getTitle())
+			activeTabIcon.attr('src', './images/spinner.gif')
+			searchBar.val(activeView[0].getURL())
+		}
 	})
 
 	// Set final favicon
-	activeView[0].addEventListener("page-favicon-updated", (e) => {
+	activeView[0].addEventListener('page-favicon-updated', (e) => {
 		// Check if favicons exists else use default favicon
 		$.get(e.favicons[0]).done(() => {
 			activeTabIcon.attr('src', e.favicons[0])
 		}).fail(() => {
-			activeTabIcon.attr('src', "./images/default-favicon.png")
+			activeTabIcon.attr('src', './images/default-favicon.png')
 		})
 	})
 
 	// Set final title & url
-	activeView[0].addEventListener("did-finish-load", () => {
+	activeView[0].addEventListener('did-finish-load', () => {
 		activeTabTitle.text(activeView[0].getTitle())
 		searchBar.val(activeView[0].getURL())
 
-		// If new tab page, empty search bar
-		if (activeView[0].getURL() == "file:///home/azwreith/Documents/GitHub/elementary-browser/app/new-tab.html") {
-			searchBar.val("")
+		// If new tab, empty search bar and set default favicon
+		if (/^file:\/\/.*new-tab/.test(activeView[0].getURL())) {
+			searchBar.val('')
+			activeTabIcon.attr('src', './images/default-favicon.png')
 		}
+
+		// If settings page, empty search bar and set settings favicon
+		else if (/^file:\/\/.*settings/.test(activeView[0].getURL())) {
+			searchBar.val('')
+			activeTabIcon.attr('src', './images/settings-favicon.png')
+		}
+
 	})
 
 }
